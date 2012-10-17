a54ac1b2535bea851615a9f6dd8f6ff9e2cb081c
nickwatts <nickwatts80@googlemail.com>
2012-10-18 00:46:31 +0100

    Added jquery ui date picker controls for Release Date fields on Create and Edit views

---
+++ MvcMovie/Scripts/add.jquery.controls.js
@@ -0,0 +1,6 @@
+﻿//<reference path="jquery-1.5.1.js" />
+//<reference path="jquery-ui-1.8.11.js" />
+$(document).ready(function () {
+  //  $('.date').datepicker({ dateFormat: "dd/mm/yy" });
+  $('#ReleaseDate').datepicker({ dateFormat: "dd/mm/yy" });
+});
